878e65729f705edcaf3e96ff4d0d87c64d19a585
DargonFly378 <hafichever@gmail.com>
2022-08-28 17:34:37 +0700

    hook-2

--- src/components/HookCounterOne.jsx
+++ src/components/HookCounterOne.jsx
@@ -2,14 +2,20 @@ import React, { useEffect, useState } from "react";
 
 const HookCounterOne = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     document.title = `You click me ${count} times`;
-    console.log(count);
-  });
+    console.log("component diupdate");
+  }, [count]);
 
   return (
     <div>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
       <button onClick={() => setCount(count + 1)}>Click {count} times</button>
     </div>
   );
